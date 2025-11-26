790b8269e2725424c84e96553e6a71983c9c72b7
AL-sama <ohyaustin192202@gmail.com>
2025-11-26 17:21:52 +0800

    Add files via upload

---
+++ code/Factors.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main(){
+	int n;
+	vector<int> factors;
+	
+	cin >> n;
+	for (int i=1; i<=sqrt(n); i++) {
+		if (n%i == 0) {
+			factors.push_back(i);
+			if (i != n/i) {
+				factors.push_back(n/i);
+			}
+		}
+	}
+	
+	sort(factors.begin(), factors.end());
+	for (int f : factors){
+		cout << f << "\n";
+	}
+}
