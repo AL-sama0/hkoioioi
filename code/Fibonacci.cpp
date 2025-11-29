7abfb2acfbcf79018a8c6e776aad17c08c47e201
AL-sama <ohyaustin192202@gmail.com>
2025-11-29 13:38:46 +0800

    Refactor Fibonacci calculation to iterative method

--- code/Fibonacci.cpp
+++ code/Fibonacci.cpp
@@ -1,13 +1,35 @@
 #include <iostream>
 using namespace std;
-int main(){
-	int ans[41] = {0};
-	ans[0] = 0;
-	ans[1] = 1;
-	for (int i=2; i<=40; i++){
-		ans[i] = ans[i-1] + ans[i-2];
-	}
-	int n;
-	cin >> n;
-	cout << ans[n];
+
+int main() {
+    long long a = 1, b = 1, c = 2;
+    int x;
+    cin >> x;
+    
+    if (x == 0) {
+        cout << 0;
+        return 0;
+    }
+    if (x == 1) {
+        cout << 1;
+        return 0;
+    }
+    if (x == 2) {
+        cout << 1;
+        return 0;
+    }
+    if (x == 3) {
+        cout << 2;
+        return 0;
+    }
+    
+    for (int i = 4; i <= x; i++) {
+        long long next = b + c;
+        a = b;
+        b = c;
+        c = next;
+    }
+    
+    cout << c;
+    return 0;
 }
